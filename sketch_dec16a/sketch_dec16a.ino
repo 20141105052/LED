f829a7a7c7c190257b45f419eb650db1c4af8d1c
20141105052 <xiaoods3on1@sina.com>
2015-12-16 18:39:09 +0800

    LED灯的闪动频率
    首先电源输出在13引脚通过在高电平中延时250毫秒和在低电平中延时时间的不同来控制LED的光暗

---
+++ sketch_dec16a/sketch_dec16a.ino
@@ -0,0 +1,17 @@
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(13,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  
+    digitalWrite(13,HIGH);
+    delay(250);//延时250毫秒
+    digitalWrite(13,LOW);
+    delay(250);
+    digitalWrite(13,HIGH);
+    delay(50);//延时50毫秒
+    digitalWrite(13,LOW);
+    delay(50);
+}
